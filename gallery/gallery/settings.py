b3108781ec48db6c52695bda13263b56dbbcd250
Memory-Wabwile <memorywabwile@gmail.com>
2022-03-26 18:29:06 +0300

    install bootstrap3 and define it in the settings file

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mems.apps.MemsConfig'
+    'mems.apps.MemsConfig',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
