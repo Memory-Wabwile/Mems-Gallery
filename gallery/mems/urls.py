5efda65da846ea13abf225c48bea3c416e75fbf5
Memory-Wabwile <memorywabwile@gmail.com>
2022-03-26 16:33:55 +0300

    define the application configurations inside the project

--- gallery/mems/urls.py
+++ gallery/mems/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path , re_path
 from . import views
 
 #urlpatterns for a list of urls in our app
 urlpatterns=[
     path('',views.welcome,name = 'welcome'),
     path('' , views.mems_home, name = 'memsHome')
+    
 ]
