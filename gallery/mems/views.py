ac8ef0574ec7bb4761fe17cb1972d62a5d5cdbc7
Memory-Wabwile <memorywabwile@gmail.com>
2022-03-26 15:24:12 +0300

    create a mems home view function

--- gallery/mems/views.py
+++ gallery/mems/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 
 
+
 # Create your views here.
 def welcome(request):
     '''
     the view function
     '''
     return HttpResponse('Welcome to Mems gallery')
+
+def mems_home(request):
+    '''
+    finction to display images on the home page
+    '''
+
+    return render(request, 'index.html', {'location':location, 'category': category,'pictures':pictures})
+                                         
+                                          
