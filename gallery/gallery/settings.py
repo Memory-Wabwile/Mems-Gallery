b0b2c99f5340779561d819d83280be75c0af5b85
Memory-Wabwile <memorywabwile@gmail.com>
2022-03-26 23:59:07 +0300

    set the database and change the timezone

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -79,6 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER' : 'memory',
+        'PASSWORD' : 'MEM99ory',
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
