3e79d5aa0aea9e1f95871c3f46cbcd2a922e7e5d
Memory-Wabwile <memorywabwile@gmail.com>
2022-03-27 15:13:34 +0300

    make migrations

--- gallery/mems/models.py
+++ gallery/mems/models.py
@@ -1,11 +1,16 @@
+from email.mime import image
 from django.db import models
 
 # Create your models here.
+
 
 class Location(models.Model):
     name : models.CharField(max_length=30)
 
     def save_location(self):
+        self.save()
+
+    def delete_location(self):
         self.save()
 
     def __str__(self):
@@ -14,9 +19,16 @@ class Location(models.Model):
 class Category(models.Model):
     name : models.CharField(max_length=30)
 
+    def save_category(self):
+        self.save()
+
     def __str__(self):
         return self.name
 
-    def save_category(self):
-        self.save()
+class Image(models.Model):
+  
+    name = models.CharField(max_length=30)
+    description = models.TextField(max_length=80)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
        
