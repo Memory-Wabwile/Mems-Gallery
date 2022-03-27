f364752d27aa652cc5c71d44572db695dbfb9cba
Memory-Wabwile <memorywabwile@gmail.com>
2022-03-27 23:01:59 +0300

    create a search template for displaying searched items

--- gallery/mems/views.py
+++ gallery/mems/views.py
@@ -23,13 +23,13 @@ def search_results(request):
 
     if 'image' in request.GET and request.GET["image"]:
         search_term = request.GET.get("image")
-        searched_articles = Image.search_by_category(search_term)
+        searched_articles = Image.search_image(search_term)
         message = f"{search_term}"
 
-        return render(request, 'image.html',{"message":message,"images": searched_articles})
+        return render(request, 'search.html',{"message":message,"images": searched_articles})
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'image.html',{"message":message})
+        return render(request, 'search.html',{"message":message})
                                          
                                           
