2e08c38c7e4b05835d4860f89dd72f31a2224c5e
Memory-Wabwile <memorywabwile@gmail.com>
2022-03-29 14:11:27 +0300

    deployment to heroku

--- gallery/settings.py
+++ gallery/settings.py
@@ -152,7 +152,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
